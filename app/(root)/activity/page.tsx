c39138d931ab6fdfa4c25e196769f552947a8554
LazY <darshitmistry2@gmail.com>
2023-08-13 09:45:58 +0530

    Activity fecthing complate and some bug fixed then Activity page done

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,44 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+// import PostThread from "@/components/forms/PostThread";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import Link from "next/link";
 
-async function Page() {
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  // get Activity
+  const activity = await getActivity(userInfo._id)
   return (
     <section>
       <h1 className="head-text">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {
+          activity.length > 0 ?(
+            <>
+            {
+              activity.map((activity) => {
+                <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                  <article className="activity-card">
+                    <Image src={activity.author.image} alt="profile pic" width={20} height={20} className="rounded-full"/>
+                  </article>
+                </Link>
+              })
+            }
+            </>
+          ) : (<p className="text-base-regular text-light-2">No Activity Yet</p>)
+        }
+      </section>
     </section>
   );
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -131,3 +131,25 @@ export async function fetchUsers({
 throw new Error (`some error occured : ${error.message}`)
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    const userThreads = await Thread.find({author:userId})
+    
+    
+    const childThreadIds = userThreads.reduce((acc , userThread) =>{
+      return acc.concat(userThread.children)
+    },[])
+    const replies = await Thread.find({
+      _id : { $in : childThreadIds } ,
+      author : { $ne : userId }
+    }).populate({
+      path :'author' ,
+      model : User,
+      select : 'name image _id'
+    })
+    return replies
+  } catch (error:any) {
+    throw new Error(`Failed to fetch Activity : ${error.message}`)
+  }
+}
